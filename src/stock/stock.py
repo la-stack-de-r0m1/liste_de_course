d64b8690e4a2806c2c18e29541a9477e0a8b747c
r0m1 <romain.bertholon@gmail.com>
2021-12-11 17:55:15 +0100

    Update the stcok with InventoryGoods

--- src/stock/stock.py
+++ src/stock/stock.py
@@ -1,5 +1,7 @@
-class Stock:
+from src.stock.inventory_good import InventoryGood
 
+
+class Stock:
     def __init__(self) -> None:
         self.goods = {}
 
@@ -10,22 +12,31 @@ class Stock:
         return good_name in self.goods
 
     def add(self, good):
-        good_name = good['name']
+        good_name = good.name
         if self.has(good_name):
-            self.goods[good_name] += 1
+            self.goods[good_name].quantity += good.quantity
         else:
-            self.create_good(good_name)
+            self.create_good(good)
 
-    def create_good(self, good_name):
-        self.goods[good_name] = 1
+    def create_good(self, good):
+        self.goods[good.name] = good
 
-    def get(self, good_name):
-        return self.goods[good_name] if self.has(good_name) else 0
+    def get_quantity(self, good_name):
+        return self.goods[good_name].quantity if self.has(good_name) else 0
 
-    def take(self, good_name):
-        if self.has(good_name=good_name):
-            if self.goods[good_name]:
-                self.goods[good_name] -= 1
-
-
+    def take(self, good):
+        returned_good = InventoryGood(good.name, good.unit, good.quantity)
+        quantity = self.compute_new_quantity(returned_good) if self.has(good.name) else 0
+        returned_good.quantity = quantity
     
+        return returned_good
+
+    def compute_new_quantity(self, good):
+        quantity = good.quantity
+        remaining_quantity = self.goods[good.name].quantity
+        if remaining_quantity >= quantity:
+            self.goods[good.name].quantity -= quantity
+        else:
+            quantity = remaining_quantity
+            self.goods[good.name].quantity = 0
+        return quantity
--- tests/stock/test_stock.py
+++ tests/stock/test_stock.py
@@ -1,4 +1,5 @@
 from src.stock.stock import Stock
+from src.stock.inventory_good import InventoryGood
 import unittest
 
 class TestStock(unittest.TestCase):
@@ -14,8 +15,8 @@ class TestStock(unittest.TestCase):
 
     def test_add_good(self):
         s = Stock()
-        s.add({'name': 'rice'})
-
+        
+        s.add(InventoryGood('rice', 'kg', 1))
         self.assertEqual(1, s.total_quantity())
 
     def test_has_not_good(self):
@@ -24,61 +25,71 @@ class TestStock(unittest.TestCase):
 
     def test_has_good(self):
         s = Stock()
-        s.add({'name': 'rice'})
+        s.add(InventoryGood('rice', 'kg', 1))
         self.assertTrue(s.has('rice'))
 
     def test_create_good(self):
         s = Stock()
         
-        s.create_good('rice')
+        s.create_good(InventoryGood('rice', 'kg', 1))
         self.assertTrue(s.has('rice'))
 
-        s.create_good('flour')
+        s.create_good(InventoryGood('flour', 'kg', 1))
         self.assertTrue(s.has('flour'))
 
     def test_count_rice_quantity_is_zero(self):
         s = Stock()
         
-        nb_rice = s.get(good_name='rice')
+        nb_rice = s.get_quantity(good_name='rice')
         self.assertEqual(0, nb_rice)
 
     def test_count_rice_quantity(self):
         s = Stock()
         
-        s.add({'name': 'rice'})
-        nb_rice = s.get(good_name='rice')
+        s.add(InventoryGood('rice', 'kg', 1))
+        nb_rice = s.get_quantity(good_name='rice')
         self.assertEqual(1, nb_rice)
 
-        s.add({'name': 'rice'})
-        nb_rice = s.get(good_name='rice')
-        self.assertEqual(2, nb_rice)
+        s.add(InventoryGood('rice', 'kg', 1.5))
+        nb_rice = s.get_quantity(good_name='rice')
+        self.assertEqual(2.5, nb_rice)
 
     def test_add_rice(self):
         s = Stock()
-        s.add({'name': 'rice'})
-        nb_rice = s.get(good_name='rice')
+        s.add(InventoryGood('rice', 'kg', 1))
+        nb_rice = s.get_quantity(good_name='rice')
         self.assertEqual(1, nb_rice)
 
     def test_add_flour(self):
         s = Stock()
-        s.add({'name': 'flour'})
-        nb_flour = s.get(good_name='flour')
+        s.add(InventoryGood('flour', 'kg', 1))
+        nb_flour = s.get_quantity(good_name='flour')
         self.assertEqual(1, nb_flour)
 
     def test_remove_flour(self):
         s = Stock()
-        s.add({'name': 'flour'})
+        s.add(InventoryGood('flour', 'kg', 2))
 
-        nb_flour = s.get(good_name='flour')
-        self.assertEqual(1, nb_flour)
+        nb_flour = s.get_quantity(good_name='flour')
+        self.assertEqual(2, nb_flour)
 
-        s.take('flour')
-        self.assertEqual(0, s.get(good_name='flour'))
+        s.take(InventoryGood('flour', 'kg', 1))
+        self.assertEqual(1, s.get_quantity(good_name='flour'))
 
     def test_stock_dont_go_negative(self):
         s = Stock()
-        s.add({'name': 'flour'})
-        s.take('flour')
-        s.take('flour')
-        self.assertEqual(0, s.get(good_name='flour'))
+        s.add(InventoryGood('flour', 'kg', 1))
+        s.take(InventoryGood('flour', 'kg', 2))
+        self.assertEqual(0, s.get_quantity(good_name='flour'))
         
+    def test_taken_good(self):
+        s = Stock()
+
+        s.add(InventoryGood('flour', 'kg', 2))
+        flour = s.take(InventoryGood('flour', 'kg', 0.5))
+        self.assertEqual(0.5, flour.quantity)
+
+    def test_empty_good_if_not_found(self):
+        s = Stock()
+        flour = s.take(InventoryGood('flour', 'kg', 0.5))
+        self.assertEqual(0.0, flour.quantity)
