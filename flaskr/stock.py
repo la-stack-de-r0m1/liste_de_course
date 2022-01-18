dddceebc8da4ce3a3eb9a6f2b07038466ac65bbd
r0m1 <romain.bertholon@gmail.com>
2022-01-18 19:05:09 +0100

    + protect stock with auth_required decorator

--- flaskr/stock.py
+++ flaskr/stock.py
@@ -1,25 +1,30 @@
 from flask import Blueprint
 from markupsafe import escape
 from flaskr.src.stock.stock_controller import StockController
+from flaskr.auth import login_required
 
 stock_controller = StockController()
 
 stock_bp = Blueprint('stock', __name__, url_prefix='/stock')
 
 @stock_bp.route("/")
+@login_required
 def stock():
     return stock_controller.index()
 
 @stock_bp.route("/add", methods=['POST', 'GET'])
+@login_required
 def add():
     return stock_controller.add()
 
 @stock_bp.route("/del/<name>", methods=['POST'])
+@login_required
 def delete(name):
     name = escape(name)
     return stock_controller.delete(name)
 
 @stock_bp.route("/<name>", methods=['POST', 'GET'])
+@login_required
 def edit(name):
     name = escape(name)
     return stock_controller.edit(name)
